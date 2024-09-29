5799061f1c6e9c1fe11dd360d861ac6e1def4736
sersawy <abdelrhmansersawy@gmail.com>
2024-09-29 23:47:12 +0300

    implement the lower common ancestor

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -20,6 +20,7 @@ public class Commit implements Serializable{
     private final String message;
     private final Vector<String> parentCommit;
     private final Map<String,String> blobs;
+    private final int depthInTree;
     public Commit(){
         timeStamp = getInitialTime();
         message = "initial commit.";
@@ -27,14 +28,16 @@ public class Commit implements Serializable{
         parentCommit = new Vector<>();
         parentCommit.add(null);
         blobs = new HashMap<>();
+        depthInTree = 0; // root of the tree
     }
     public Commit(Commit other){
-        // Clone commit
+        // Clone Object
         this.timeStamp = other.timeStamp;
         this.branchName = other.branchName;
         this.message = other.message;
         this.parentCommit = other.parentCommit;
         this.blobs = other.blobs;
+        this.depthInTree = other.getDepthInTree();
     }
     public Commit(Commit parent , String branchName , String message){
         this.timeStamp = getCurrentTime();
@@ -43,8 +46,10 @@ public class Commit implements Serializable{
         this.parentCommit = new Vector<>();
         this.parentCommit.add(parent.getCommitSHA());
         this.blobs = parent.blobs;
+        this.depthInTree = 1 + parent.getDepthInTree();
     }
     public Commit(StagingArea currentStagingArea , String branchName , String message){
+       // Constructor for commit command
         this(currentStagingArea.getHead() , branchName , message);
         for(Map.Entry<String,String> entry: currentStagingArea.getstagingForAddional().entrySet()){
             String fileName = entry.getKey();
@@ -75,6 +80,7 @@ public class Commit implements Serializable{
         // to get the SHA1 of the object
         return sha1(timeStamp,branchName,message);
     }
+    public int getDepthInTree(){ return this.depthInTree; }
     public Map<String , String > getBlobs(){ return this.blobs; };
     public String getBlobName(String fileName){
         assert blobs.containsKey(fileName);
@@ -113,12 +119,44 @@ public class Commit implements Serializable{
         System.out.println("Date: " + getTimeStamp());
         System.out.println(getMessage());
     }
-    public void merge(Map <String , String >blob , String par1 , String par2, String branchName)
-    {
-        message = "message";
-        this.blobs = blob;
-        this.parentCommit.add(par2);
-        this.parentCommit.add(par1);
-        this.branchName = branchName;
+
+    // Merging two branches
+    private String move(int steps){
+        String currentCommit = getCommitSHA();
+        while(steps > 0){
+            currentCommit  = Commit.read(currentCommit).getParentSHA();
+            steps--;
+        }
+        return currentCommit;
     }
+    public String getLowestCommonCommit(String commitID1 , String commitID2){
+        Commit c1 = Commit.read(commitID1);
+        Commit c2 = Commit.read(commitID2);
+        // Move the two commit to the same depth into the tree
+        if(c1.getDepthInTree() > c2.getDepthInTree()){
+            commitID1 = c1.move(c1.getDepthInTree() - c2.getDepthInTree());
+        }else{
+            commitID2 = c2.move(c2.getDepthInTree() - c1.getDepthInTree());
+        }
+        // Move every commit to its parentCommit until they go into the same commit;
+        while (!Objects.equals(commitID1, commitID2)){
+            commitID1 = Commit.read(commitID1).getParentSHA();
+            commitID2 = Commit.read(commitID2).getParentSHA();
+        }
+        return commitID1;
+    }
+//    public Commit(Commit parentCommit1 , Commit parentCommit2 , String message){
+//        this.message = message;
+//        this.parentCommit = new Vector<>();
+//        this.parentCommit.add(parentCommit2.getCommitSHA()); // merged Branch
+//        this.parentCommit.add(parentCommit1.getCommitSHA()); // current working Branch
+//        this.branchName = parentCommit1.getBranchName();
+//    }
+//    public void merge(Map <String , String >blob , String par1 , String par2, String branchName) {
+//        this.message = "message";
+//        this.blobs = blob;
+//        this.parentCommit.add(par2);
+//        this.parentCommit.add(par1);
+//        this.branchName = branchName;
+//    }
 }
